1bf138df3d9df0bccd2aca16922a27f2b6db45ba
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-05-30 16:27:07 +0530

    music recomendation system :)

---
+++ precache-manifest.75005bee7705898ab488b9c73bb0d59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c8f7023ff2f98185fd910d336d1ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "084f35a2078487e4f4bd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99995b315d5340911951",
+    "url": "/static/js/2.7184bed9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7184bed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084f35a2078487e4f4bd",
+    "url": "/static/js/main.bead014d.chunk.js"
+  },
+  {
+    "revision": "2c81e5c5b636d89f56f0",
+    "url": "/static/js/runtime-main.2253721a.js"
+  }
+]);
